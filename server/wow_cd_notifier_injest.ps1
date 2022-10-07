dfb925d0503b87bd858d19f7a6a6124157acf9b0
El Brento <ninthwalker@gmail.com>
2022-10-07 14:41:34 -0600

    Add files via upload

--- server/wow_cd_notifier_injest.ps1
+++ server/wow_cd_notifier_injest.ps1
@@ -1,4 +1,4 @@
-# WOW Cooldown Alerter
+﻿# WOTLK Cooldown Alerter
 # Author: Ninthwalker
 # Last Modified: 06OCT2022
 # Used in conjunction with this WA: https://wago.io/sluyr3nQ8
@@ -6,8 +6,6 @@
 # Run this as a Windows Scheduled Task
 # run it to check peoples alerts every 10min. Code below is set for this 10 min interval. May need to adjust slightly for a buffer window as it could miss if its too exact possibly.
 # task arguments: -executionpolicy bypass -noprofile -nologo -windowstyle hidden -File "..\wow_cd_notifier_injest.ps1"
-# using this upload docker. could replace this and use whatever upload method is preferable though.
-# https://github.com/ninthwalker/docker-simple-file-upload
 ##################################
 
 #####################################
@@ -60,13 +58,13 @@ foreach ($cd in $cdInfo) {
     if ( ($diff.TotalMinutes -ge $bugMe) -and ($diff.TotalMinutes -le $cd.alertTime) ) {
         # Send discord alert. Less than $alertTime until CD is ready! Also need to check if there is no time or its off cd type situation. -UFormat %r if we just want the time and not date. Time is UTC now to use UTC from remote computer.
         # newline also works with: $(0x0A -as [char])
-        if ( ($cd.keepBuggingMe -eq $True) -and ($diff -le 0) -and ($cd.interval -eq $True) -and ($diff.TotalMinutes%$cd.intervalTime -le 10)) {
+        if ( ($cd.keepBuggingMe -eq $True) -and ($diff -le 0) -and ($cd.interval -eq $True) -and ($diff.TotalMinutes%$cd.intervalTime -ge 10)) {
             New-Alert -discordId $cd.discordId -icon $cd.Icon -title "$($cd.name) Cooldown is ready!" -color "GREEN" -msg "**Time is money, friend!**$(0x0A -as [char])**Was Ready At:** $(Get-Date $localTime -F g)" -link $cd.link -realm $cd.Realm -char $cd.char
         }
-        elseif ( ($cd.interval -eq $True) -and ($diff.TotalMinutes%$cd.intervalTime -le 10) ) {
+        elseif ( ($cd.interval -eq $True) -and ($diff -ge 0) -and ($diff.TotalMinutes -le $cd.alertTime) -and ($diff.TotalMinutes%$cd.intervalTime -le 10) ) {
             New-Alert -discordId $cd.discordId -icon $cd.Icon -title "$($cd.name) Cooldown is almost ready!" -color "YELLOW" -msg "**Cooldown ready in:** $( if($diff.days -gt 0) {"$($diff.days)d "}) $( if($diff.hours -gt 0) {"$($diff.hours)h "}) $( if($diff.minutes -gt 0) {"$($diff.minutes)m"}) $( if(($diff.minutes -le 0) -and ($diff.hours -le 0) -and ($diff.days -le 0)){"$($diff.seconds)s"})$(0x0A -as [char])**Ready At:** $(Get-Date $localTime -F g)" -link $cd.link -realm $cd.Realm -char $cd.char
         }
-        elseif (($diff.TotalMinutes -le $cd.alertTime) -and ($diff.TotalMinutes -ge ($cd.alertTime -10))) {
+        elseif (($diff.TotalMinutes -le $cd.alertTime) -and ($diff.TotalMinutes -ge ($cd.alertTime - 10))) {
             New-Alert -discordId $cd.discordId -icon $cd.Icon -title "$($cd.name) Cooldown is almost ready!" -color "YELLOW" -msg "**Cooldown ready in:** $( if($diff.days -gt 0) {"$($diff.days)d "}) $( if($diff.hours -gt 0) {"$($diff.hours)h "}) $( if($diff.minutes -gt 0) {"$($diff.minutes)m"}) $( if(($diff.minutes -le 0) -and ($diff.hours -le 0) -and ($diff.days -le 0)){"$($diff.seconds)s"})$(0x0A -as [char])**Ready At:** $(Get-Date $localTime -F g)" -link $cd.link -realm $cd.Realm -char $cd.char
         }
         else {
